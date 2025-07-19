2460b8f6e841493403f8cd862d81bf6589fffa9d
chez1s <toptrainghieml@gmail.com>
2025-07-19 22:15:19 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,60 @@
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
+  <div class="min-h-screen flex flex-col">
+    <div class="navbar bg-base-100 shadow-sm">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+        </div>
+        <a class="btn btn-ghost text-xl"
+          ><router-link to="/home">cz-learning-vuejs</router-link></a
+        >
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+          <li><router-link to="/home">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+        </ul>
+      </div>
+      <div class="navbar-end">
+        <div class="avatar">
+          <div class="w-12 rounded-full">
+            <img
+              src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
             />
-          </svg>
-        </div>
-      </div>
-      <a class="btn btn-ghost text-xl"
-        ><router-link to="/home">cz-learning-vuejs</router-link></a
-      >
-    </div>
-    <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><router-link to="/home">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-      </ul>
-    </div>
-    <div class="navbar-end">
-      <div class="avatar">
-        <div class="w-12 rounded-full">
-          <img
-            src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
-          />
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <router-view></router-view>
+    <main class="flex-grow">
+      <router-view></router-view>
+    </main>
 
-  <div>
-    <h1>Cai l nay la footer</h1>
-    <!-- <p>This is a simple Vue.js application.</p> -->
+    <footer
+      class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4"
+    >
+      <aside>
+        <p>Copyright © {{ currentYear }} - All rights reserved by yeumeent</p>
+      </aside>
+    </footer>
   </div>
 </template>
+
+<script setup>
+const currentYear = new Date().getFullYear();
+</script>
